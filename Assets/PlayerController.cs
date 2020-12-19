901eb46365501e283c5d09a1c159aec6aec5cc75
Ursula <edenben13@hotmail.com>
2020-12-19 12:37:58 +0200

    blah

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,24 +12,33 @@ public class PlayerController : MonoBehaviour
 
     public float sensitivity = 10f;
 
-    public float walking = 12f;
+    public float walking;
     public float running = 0f;
+    public float jumpStrength;
     public float speed;
 
-    bool IsHandFull;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    bool IsHandFull, IsOnGround;
+    Vector3 PlayerVelocity;
+
+    public Vector3 RespawnPoint;
+
+    public CapsuleCollider PlayerCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         IsHandFull = false;
+        RespawnPoint = transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        ControlWalking();
-
-        ControlRuning();
+    {        
+        Jump();
 
         ControlCamera();
 
@@ -81,38 +90,87 @@ public class PlayerController : MonoBehaviour
 
     public void ControlRuning()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            running = 8f;
+            speed += running;
         }
-        else
-        {
-            running = 0f;
-        }
+
 
     }
 
     public void Crawling()
     {
-        if (isOnGround)
+        if (IsOnGround)
         {
-            if (Input.GetKey(KeyCode.C))
+            if (Input.GetKey(KeyCode.Z))
             {
-                whatever.height = 0.5f;
+                PlayerCollider.height = 0.5f;
             }
             else
             {
-                whatever.height = 1f;
+                PlayerCollider.height = 1f;
             }
         }
     }
 
     public void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isOnGround)
+        speed = walking;
+
+        ControlRuning();
+
+        IsOnGround = controller.isGrounded;
+        if (IsOnGround && PlayerVelocity.y < 0)
         {
-            //rb.AddForce(new Vector3(0, 6f, 0), ForceMode.Impulse);
-            isOnGround = false;
+            PlayerVelocity.y = 0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = Ground.transform.right * x + Ground.transform.forward * z;
+
+        //if (move != Vector3.zero)
+        //{
+        //    gameObject.transform.forward = move;
+        //}
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && IsOnGround)
+        {
+            PlayerVelocity.y += Mathf.Sqrt(jumpHeight * -jumpStrength * gravityValue);
+        }
+
+        PlayerVelocity.x = move.x * speed;
+        PlayerVelocity.z = move.z * speed;
+
+        PlayerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(PlayerVelocity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Rope")
+        {
+            other.GetComponentInParent<RopeController>().TouchingPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Rope")
+        {
+            other.GetComponentInParent<RopeController>().TouchingPlayer = false;
+        }
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Spikes")
+        {
+            transform.position = RespawnPoint;
+            IsOnGround = true;
         }
     }
 }
---
+++ Assets/RopeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour
+{
+    public bool TouchingPlayer;
+
+    public Transform SpawnPoint;
+
+    public PlayerController Player;
+
+    public Material Mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mat.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (TouchingPlayer)
+        {
+            Mat.color = Color.green;
+        }
+    }
+
+
+    private void OnMouseEnter()
+    {
+        if (true)
+        {
+            Mat.color = Color.white;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (TouchingPlayer)
+        {
+            Player.controller.enabled = false;
+            Player.transform.position = SpawnPoint.position;
+            Player.RespawnPoint = SpawnPoint.position;
+            Player.controller.enabled = true;
+            Mat.color = Color.white;
+            TouchingPlayer = false;
+        }
+    }
+}
